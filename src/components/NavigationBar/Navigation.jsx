12cbab2566561e7c11103fe327b4c200f5b2cbda
dev-himanshu-01 <himanshu01.dev@gmail.com>
2023-06-04 13:04:49 -0700

    clean-up && mesh-gradient-added ✔

---
+++ src/components/NavigationBar/Navigation.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navigation = () => {
+  return <div>NavigationBar it is!?</div>;
+};
+
+export default Navigation;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Navigation from "@/components/NavigationBar/Navigation";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navigation />
+      <Component {...pageProps} />
+    </>
+  );
 }
