e0417e8fd06cf5df98b5af3aa80fb8046f328a37
dev-himanshu-01 <himanshu01.dev@gmail.com>
2023-06-08 16:26:41 -0700

    GuestBook Page :done

--- src/pages/guestbook.jsx
+++ src/pages/guestbook.jsx
@@ -1,11 +1,32 @@
 import React from "react";
+import styles from "@/styles/Guestbook.module.css";
+import Giscus from "@giscus/react";
+import Footer from "@/components/Footer/Footer";
 import Navigation from "@/components/NavigationBar/Navigation";
 
 const guestbook = () => {
   return (
     <>
       <Navigation currentPage="Guestbook" />
-      <div>THIS IS GUESTBOOK PAGE!</div>
+      <section className={styles.Main}>
+        <h1>GUESTBOOK</h1>
+        <p>Leave a message :)</p>
+
+        <div className={styles.Container}>
+          <Giscus
+            key={"Comments"}
+            repo="garadiya0/garadiya0"
+            repoId="R_kgDOHymOVw"
+            category="General"
+            mapping="url"
+            reactionsEnabled="1"
+            emitMetadata="0"
+            theme="dark"
+            async
+          />
+        </div>
+      </section>
+      <Footer/>
     </>
   );
 };
