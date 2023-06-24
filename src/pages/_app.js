1b792af4d46bdf66bbfd4d65faee373608c7ec7e
Himanshu <himanshu.p@hipla.io>
2023-06-24 21:49:52 +0530

    Head-Tag moved to _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,25 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+    <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap"
+          rel="stylesheet"
+        />
+
+        <title>Himanshu Garadiya | Developer</title>
+        <meta
+          name="description"
+          content="Personal Portfolio of Himanshu Garadiya"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </>
   );
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,22 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap"
-          rel="stylesheet"
-        />
-
-        <title>Himanshu Garadiya | Developer</title>
-        <meta
-          name="description"
-          content="Personal Portfolio of Himanshu Garadiya"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Head/>
       <body>
         <Main />
         <NextScript />
