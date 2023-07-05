330d08ce78b7d865ac1d8a03a7095b53c8cc0ca4
himanshu <himanshu01.dev@gmail.com>
2023-07-05 13:01:55 +0530

    firebase-config & .env updated

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,20 +1,18 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore/lite";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAUTJOsGXKcFldE9YpIA0GzEOMvYDR1Ow8",
-  authDomain: "codexhimanshu-01.firebaseapp.com",
-  projectId: "codexhimanshu-01",
-  storageBucket: "codexhimanshu-01.appspot.com",
-  messagingSenderId: "151419451937",
-  appId: "1:151419451937:web:2d6c6ff69be9ec1756c160",
-  measurementId: "G-DJ972JLVDE",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 
 // Initialize Firestore
 const database = getFirestore(app);
